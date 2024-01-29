58cb774006ec9961a79a3a5c0fb46795b7d55876
ovichowdhury <ovi.chowdhury@hotmail.com>
2024-01-29 16:15:08 +0600

    feat: response type added for swagger

--- src/notes/entities/note.entity.ts
+++ src/notes/entities/note.entity.ts
@@ -1,1 +1,19 @@
-export class Note {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Notes } from '@prisma/client';
+
+export class NoteEntity implements Notes {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  desc: string | null;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -10,37 +10,51 @@ import {
 import { NotesService } from './notes.service';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { NoteEntity } from './entities/note.entity';
 
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
 
   @Post()
+  @ApiTags('Notes')
+  @ApiCreatedResponse({ type: NoteEntity })
   create(@Body() createNoteDto: CreateNoteDto) {
     return this.notesService.create(createNoteDto);
   }
 
   @Get()
+  @ApiTags('Notes')
+  @ApiCreatedResponse({ type: NoteEntity, isArray: true })
   findAll() {
     return this.notesService.findAll();
   }
 
   @Get('today')
+  @ApiTags('Notes')
+  @ApiCreatedResponse({ type: NoteEntity, isArray: true })
   findToday() {
     return this.notesService.findToday();
   }
 
   @Get(':id')
+  @ApiTags('Notes')
+  @ApiCreatedResponse({ type: NoteEntity })
   findOne(@Param('id') id: string) {
     return this.notesService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiTags('Notes')
+  @ApiCreatedResponse({ type: NoteEntity })
   update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
     return this.notesService.update(+id, updateNoteDto);
   }
 
   @Delete(':id')
+  @ApiTags('Notes')
+  @ApiCreatedResponse({ type: NoteEntity })
   remove(@Param('id') id: string) {
     return this.notesService.remove(+id);
   }
